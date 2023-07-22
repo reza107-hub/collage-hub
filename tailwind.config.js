68f502e6bbc149b79b12aa308a7bedb99d80e766
reza107-hub <rashidaminur104@gmail.com>
2023-07-22 08:46:10 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+const colors = require('tailwindcss/colors')
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      'main': '#39CB8A',
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      emerald: colors.emerald,
+      indigo: colors.indigo,
+      yellow: colors.yellow,
+    },
+  },
+  daisyui: {
+    themes: [],
+  },
+  plugins: [require("daisyui")],
+}
